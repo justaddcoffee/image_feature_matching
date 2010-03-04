73667702e9cac062c3e4b4cae58d5af9c8acceb9
Justin Reese <justaddcoffee+git@gmail.com>
2010-03-04 23:25:12 +0700

    finished working demo of extract_contours, which writes all contours out to a file

--- extract_contours.c
+++ extract_contours.c
@@ -8,6 +8,9 @@ writes out
    image_file_contours_2
    image_file_contours_3
 
+Problems: 
+- OpenCV doesn't seem to support bounding boxes for Freeman Chain-type contours, which I think we'll need to use. Might need to compute bounding box manually? (Why doesn't OpenCV support this?)
+
 */
 
 #define DEBUG 0
@@ -26,6 +29,9 @@ writes out
 #include <cxcore.h>
 #include <highgui.h>
 #include <stdio.h>
+
+IplImage *CopySubImage(IplImage *full_image,int x_top_left, int y_top_left, int width, int height);
+int write_image_file( IplImage *image_to_write, char *file_name );
 
 IplImage*	image_original = NULL;
 IplImage*	image_manip = NULL;
@@ -71,38 +77,111 @@ int main(int argc, char** argv){
   cvShowImage( "Thresholded", image_manip );
 #endif
     
-  int contoursFound = 0;
-
+  int numContoursFound = 0;
   //cvFindContours( src, storage, &contour, sizeof(CvContour), CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
-
-  contoursFound = cvFindContours( 
+  numContoursFound = cvFindContours( 
 				 image_manip, 
 				 contour_storage, 
 				 &contours,
 				 sizeof(CvContour),
 				 CV_RETR_CCOMP,
-				 CV_CHAIN_CODE
+				 CV_CHAIN_APPROX_SIMPLE //CV_CHAIN_APPROX_SIMPLE //CV_CHAIN_CODE
 				  );
 
-  fprintf(stderr, "Found %i contours in image file '%s'\n", contoursFound, file_name); 
+  fprintf(stderr, "Found %i contours in image file '%s'\n", numContoursFound, file_name); 
 
-  // zero out image, then draw contours
-  cvZero( image_manip );
-  if( contours ){
-    cvDrawContours(
-		   image_manip,
-		   contours,
-		   cvScalarAll(255),
-		   cvScalarAll(255),
-		   100 );
+  int n=0; 
+  for( CvSeq* c=contours; c != NULL; c = c->h_next ) {
+
+#if DEBUG
+      cvDrawContours(
+		   image_original, 
+		   c, 
+		   CV_RGB(255,50,50), // cvScalarAll(0),
+		   CV_RGB(255,50,50), //cvScalarAll(255),
+		   0, // how deep into contour tree to print
+		   1.5,
+		   1
+		   ); 
+#endif
+
+    // get box around contour
+    CvRect bbs;
+    CvPoint pt_upper_left, pt_lower_right;
+    bbs = cvBoundingRect(c);
+    pt_lower_right.x = bbs.x;
+    pt_lower_right.y = bbs.y;
+    pt_upper_left.x = pt_lower_right.x + bbs.width;
+    pt_upper_left.y = pt_lower_right.y + bbs.height;
+
+#if DEBUG
+    cvRectangle( 
+		image_original,
+		pt_upper_left, 
+		pt_lower_right, 
+		CV_RGB(255,50,50), 
+		1,
+		8,
+		0
+		 );
+#endif
+
+    IplImage* extracted_contour;
+    //CopySubImage(IplImage *full_image,int x_top_left, int y_top_left, int width, int height);
+    extracted_contour = CopySubImage( image_original, bbs.x, bbs.y, bbs.width, bbs.height);
+
+    char contour_file_name[(int) strlen(file_name)];
+    //char output_winname[(int) strlen(file)];
+    sprintf (contour_file_name, "%s_contour_%i.jpg", file_name, n );
+    write_image_file(
+		     extracted_contour,
+		     contour_file_name
+		     );
+
+#if DEBUG
+    printf("Contour number %d\n", n );
+    cvShowImage( "Image", image_original );
+    printf(" %d elements:\n", c->total );
+#endif
+
+    n++;
   }
 
-  #if DEBUG
-    cvShowImage( "Contours", image_manip );
+#if DEBUG
+    cvShowImage( "Contours", image_original );
     cvWaitKey();  
-  #endif
+#endif
 
   return 0;
 
 }
 
+IplImage *CopySubImage(IplImage *full_image,int x_top_left, int y_top_left, int width, int height) {
+
+  CvRect roi;
+  IplImage *sub_image;
+  
+  roi.x = x_top_left;
+  roi.y = y_top_left;
+  roi.width = width;
+  roi.height = height;
+  
+  cvSetImageROI(full_image,roi);
+  sub_image = cvCreateImage( cvSize(roi.width, roi.height),
+			    full_image->depth, full_image->nChannels );
+  cvCopy(full_image,sub_image);
+  cvResetImageROI(full_image);
+  return sub_image;
+
+}
+ 
+int write_image_file( IplImage *image_to_write, char *file_name ){
+
+  if(cvSaveImage( file_name, image_to_write, NULL )){
+    return 1;
+  } else{
+    printf("Could not save: %s\n", file_name);
+    return 0;
+  }
+
+}
