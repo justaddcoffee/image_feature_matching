e02dd55717f40a4df6d20cbf6b8f28776a2f3de6
Justin Reese <justaddcoffee+git@gmail.com>
2010-03-04 20:30:22 +0700

    minor tweaks to extact_contours

--- extract_contours.c
+++ extract_contours.c
@@ -10,8 +10,7 @@ writes out
 
 */
 
-#define DEBUG 1
-
+#define DEBUG 0
 #define THRESHOLD_TYPE CV_THRESH_BINARY
 // CV_THRESH_BINARY or CV_THRESH_BINARY_INV
 
@@ -34,45 +33,58 @@ int		threshold = 100; // only used for regular (not adaptive) thresholding
 CvMemStorage* 	contour_storage = NULL;
 
 int main(int argc, char** argv){
-
+  
   if (argc != 2){
     printf("usage: program [image file]\n");
     exit(-42);
   }
-
+  
   const char* file_name = argv[1];
   if( ( image_original = cvLoadImage( file_name ) ) == 0 ) {
-    printf("Error reading template image file '%s'\n", file_name); 
+    fprintf(stderr, "Error reading template image file '%s'\n", file_name); 
     return(-1);
   }
-
-  #if DEBUG
-    cvNamedWindow( "Original", 1);
-    cvShowImage( "Original", image_original );
-    cvNamedWindow( "Contours", 1 );
-  #endif
-
+  
+#if DEBUG
+  cvNamedWindow( "Original", 1);
+  cvShowImage( "Original", image_original );
+  cvNamedWindow( "Contours", 1 );
+#endif
+  
   ////////////////////////////
   // start manipulating image
   ////////////////////////////
   image_manip = cvCreateImage( cvGetSize( image_original ), 8, 1 );
   contour_storage = cvCreateMemStorage(0);
-  CvSeq* contours = 0;
-
+  CvSeq* contours = NULL;
+  
   // convert to grayscale
   cvCvtColor( image_original, image_manip, CV_BGR2GRAY );
-
+  
   // make contour-ready image using thresholding or canny edge detection 
-  cvThreshold( image_manip, image_manip, threshold, 255, THRESHOLD_TYPE );
+  //cvThreshold( image_manip, image_manip, threshold, 255, THRESHOLD_TYPE );
   //cvAdaptiveThreshold(image_manip, image_manip, 255, ADAPTIVE_METHOD, THRESHOLD_TYPE, BLOCK_SIZE, SUBTRACT_PARAM);
-  //cvCanny( image_manip, image_manip, 50, 900, 3 ); // these seem to exclude a lot of crap
+  cvCanny( image_manip, image_manip, 50, 900, 3 ); // these seem to exclude a lot of crap
+  
+#if DEBUG
+  cvNamedWindow( "Thresholded", 1);
+  cvShowImage( "Thresholded", image_manip );
+#endif
+    
+  int contoursFound = 0;
 
-  #if DEBUG
-     cvNamedWindow( "Thresholded", 1);
-     cvShowImage( "Thresholded", image_manip );
-  #endif
+  //cvFindContours( src, storage, &contour, sizeof(CvContour), CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
 
-  cvFindContours( image_manip, contour_storage, &contours );
+  contoursFound = cvFindContours( 
+				 image_manip, 
+				 contour_storage, 
+				 &contours,
+				 sizeof(CvContour),
+				 CV_RETR_CCOMP,
+				 CV_CHAIN_CODE
+				  );
+
+  fprintf(stderr, "Found %i contours in image file '%s'\n", contoursFound, file_name); 
 
   // zero out image, then draw contours
   cvZero( image_manip );
